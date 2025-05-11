7cea1071409a1790d3a73e9582c68b39df00e7c1
omar switi <omarswiti10@gmail.com>
2025-05-11 20:12:36 +0300

    SQL

--- vitalindex/settings.py
+++ vitalindex/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vitalindex_db',
         'USER': 'root',
-        'PASSWORD': 'vital123s',
+        'PASSWORD': os.getenv("PASSWORD"),
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
