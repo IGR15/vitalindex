f3291e175157c5a99f39799fbbc4464e7dcf4d2d
omar switi <omarswiti10@gmail.com>
2025-05-20 19:31:36 +0300

    url fix

--- users/api/urls.py
+++ users/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # JWT Auth
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/users/logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 
     # User APIs
