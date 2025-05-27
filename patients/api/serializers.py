78fd194c325b43580d260efc361f73697bbf5ee3
omar switi <omarswiti10@gmail.com>
2025-05-27 15:57:44 +0300

    fix

--- patients/api/serializers.py
+++ patients/api/serializers.py
@@ -2,10 +2,7 @@ from rest_framework import serializers
 from patients.models import Patient
 
 class PatientSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='name')  
-
+    name = serializers.CharField()  
     class Meta:
         model = Patient
         fields = ['id', 'name', 'gender', 'address', 'phone', 'email', 'medical_history', 'date_of_birth']
-
-        
