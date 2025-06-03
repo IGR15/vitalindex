caf25affd6f470e0899954c934b96d47c30619c8
omar switi <omarswiti10@gmail.com>
2025-06-03 15:31:11 +0300

    ff

--- users/api/views.py
+++ users/api/views.py
@@ -25,7 +25,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        token['role'] = user.role.name if user.role else ('Admin' if user.is_superuser else None)
+        token['role'] = user.role if user.role else ('Admin' if user.is_superuser else None)
         token['username'] = user.username
         token['email'] = user.email
         return token
@@ -34,7 +34,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         try:
             data = super().validate(attrs)
             data.update({
-                'role': self.user.role.name if self.user.role else ('Admin' if self.user.is_superuser else None),
+                'role': self.user.role if self.user.role else ('Admin' if self.user.is_superuser else None),
                 'username': self.user.username,
                 'email': self.user.email
             })
